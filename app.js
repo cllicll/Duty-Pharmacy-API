ccae7536f2bb02705847b2c03167235b19e2548d
Mehmet COŞKUN <mehmet@coskun.dev>
2020-07-07 12:25:25 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+const { ApolloServer } = require('apollo-server-express');
+const { importSchema } = require('graphql-import');
+
+const resolvers = require('./graphql/resolvers');
+
+const API_URI = 'https://www.haberturk.com/nobetci-eczaneler/{0}/{1}';
+
+const server = new ApolloServer({
+    typeDefs: importSchema('./graphql/schema.graphql'),
+    resolvers,
+    context: {
+        API_URI,
+		fetch,
+		cheerio,
+    },
+    introspection: true,
+    playground: true,
+});
+
+const app = express();
+
+app.get('/:city/:town', async (req, res) => {
+    var city = req.params.city;
+    var town = req.params.town;
+
+    var datas = [];
+    await fetch(API_URI.replace('{0}', city).replace('{1}', town))
+        .then((response) => response.text())
+        .then((body) => {
+            const $ = cheerio.load(body);
+
+            $('figure').each(function (i, elem) {
+                datas[i] = {
+                    city: city.charAt(0).toUpperCase() + city.slice(1),
+                    town: town.charAt(0).toUpperCase() + town.slice(1),
+                    name: $(this).find('div[class=title] h3 a span').text(),
+                    address: $(this)
+                        .find('figure figcaption p')
+                        .first()
+                        .text()
+                        .split('Adres: ')[1],
+                    phone: $(this)
+                        .find('figure figcaption p')
+                        .last()
+                        .text()
+                        .split('Telefon: ')[1],
+                };
+            });
+        });
+    res.send(datas);
+});
+
+server.applyMiddleware({
+    app,
+    path: '/graphql',
+    cors: {
+        origin: true,
+        credentials: true,
+    },
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(
+        `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
+    );
+});
---
+++ graphql/resolvers/queries/Query.js
@@ -0,0 +1,35 @@
+const Query = {
+    getdutypharmacy: async (parent, args, { API_URI, fetch, cheerio }) => {
+        var datas = [];
+        await fetch(API_URI.replace('{0}', args.city).replace('{1}', args.town))
+            .then((response) => response.text())
+            .then((body) => {
+                const $ = cheerio.load(body);
+
+                $('figure').each(function (i, elem) {
+                    datas[i] = {
+                        city:
+                            args.city.charAt(0).toUpperCase() +
+                            args.city.slice(1),
+                        town:
+                            args.town.charAt(0).toUpperCase() +
+                            args.town.slice(1),
+                        name: $(this).find('div[class=title] h3 a span').text(),
+                        address: $(this)
+                            .find('figure figcaption p')
+                            .first()
+                            .text()
+                            .split('Adres: ')[1],
+                        phone: $(this)
+                            .find('figure figcaption p')
+                            .last()
+                            .text()
+                            .split('Telefon: ')[1],
+                    };
+                });
+            });
+        return datas;
+    },
+};
+
+module.exports = Query;
