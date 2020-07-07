5a3ca846df0a59ffe60170a81493126b1b87c028
Mehmet COŞKUN <mehmet@coskun.dev>
2020-07-07 13:31:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { importSchema } = require('graphql-import');
 const resolvers = require('./graphql/resolvers');
 
 const API_URI = 'https://www.haberturk.com/nobetci-eczaneler/{0}/{1}';
+const API_CITY_URI = 'https://www.haberturk.com/nobetci-eczaneler/{0}';
 
 const server = new ApolloServer({
     typeDefs: importSchema('./graphql/schema.graphql'),
@@ -22,6 +23,36 @@ const server = new ApolloServer({
 });
 
 const app = express();
+
+app.get('/:city', async (req, res) => {
+    var city = req.params.city;
+
+    var datas = [];
+    await fetch(API_CITY_URI.replace('{0}', city))
+        .then((response) => response.text())
+        .then((body) => {
+            const $ = cheerio.load(body);
+
+            $('figure').each(function (i, elem) {
+                datas[i] = {
+                    city: city.charAt(0).toUpperCase() + city.slice(1),
+                    town: town.charAt(0).toUpperCase() + town.slice(1),
+                    name: $(this).find('div[class=title] h3 a span').text(),
+                    address: $(this)
+                        .find('figure figcaption p')
+                        .first()
+                        .text()
+                        .split('Adres: ')[1],
+                    phone: $(this)
+                        .find('figure figcaption p')
+                        .last()
+                        .text()
+                        .split('Telefon: ')[1],
+                };
+            });
+        });
+    res.send(datas);
+});
 
 app.get('/:city/:town', async (req, res) => {
     var city = req.params.city;
